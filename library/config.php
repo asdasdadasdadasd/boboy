8728743c1933d714177fbad47c2417c9e49607a5
jacotabio <32643675+jacotabio@users.noreply.github.com>
2017-11-23 19:06:22 +0800

    asd

--- library/config.php
+++ library/config.php
@@ -2,13 +2,13 @@
 session_start();
 
 // config control 
-$hosted = true;
+$hosted = false;
 
 if($hosted){
   define('DB_SERVER','52.77.240.48');
   define('DB_USERNAME','admin');
   define('DB_PASSWORD','howtoforge');
-  define('DB_DATABASE','db_sleepnot');
+  define('DB_DATABASE','db_sleepnotgo');
 }else{
   define('DB_SERVER','localhost');
   define('DB_USERNAME','root');
