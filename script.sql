ab74517ed7aa22e040652285e5a178c1fea2f2a8
jacotabio <32643675+jacotabio@users.noreply.github.com>
2017-12-20 19:30:53 +0800

    DB script updated

--- script.sql
+++ script.sql
@@ -42,8 +42,8 @@ CREATE TABLE IF NOT EXISTS `brands` (
 -- Dumping data for table db_sleepnotgo.brands: ~2 rows (approximately)
 /*!40000 ALTER TABLE `brands` DISABLE KEYS */;
 INSERT INTO `brands` (`brand_id`, `brand_name`, `brand_status`, `update_checker`) VALUES
-	(14, 'SleepNot', 0, 1),
-	(15, 'Starbucks', 0, 1);
+	(14, 'SleepNot', 1, 1),
+	(15, 'Starbucks', 1, 1);
 /*!40000 ALTER TABLE `brands` ENABLE KEYS */;
 
 -- Dumping structure for table db_sleepnotgo.cart
@@ -54,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `cart` (
   `subtotal` float(10,2) NOT NULL DEFAULT '0.00',
   `usr_id` int(8) NOT NULL DEFAULT '0',
   PRIMARY KEY (`cart_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10000104 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=10000140 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table db_sleepnotgo.cart: 0 rows
 /*!40000 ALTER TABLE `cart` DISABLE KEYS */;
@@ -77,12 +77,11 @@ CREATE TABLE IF NOT EXISTS `items` (
 -- Dumping data for table db_sleepnotgo.items: ~7 rows (approximately)
 /*!40000 ALTER TABLE `items` DISABLE KEYS */;
 INSERT INTO `items` (`item_id`, `brand_id`, `item_name`, `item_description`, `item_size`, `item_price`, `item_img`, `item_status`, `created_at`) VALUES
-	(16, 14, 'Hot Choco', 'Hot chocolate, also known as hot cocoa, drinking chocolate or just cocoa is a heated beverage consisting of shaved chocolate, melted chocolate or cocoa powder, heated milk or water, and usually a sweetener.', '', 65.00, 'hot choco.jpg', 1, '2017-12-07 10:49:10'),
 	(17, 14, 'Iced Coffee', 'Iced coffee is cold coffee with ice. The iced latte and iced mocha are examples. There are various brewing methods, with the fundamental division being cold brew.', '', 45.00, 'sleepnot.jpg', 1, '2017-12-07 11:05:59'),
 	(18, 14, 'Coffee Latte', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin in nisl non vulputate. Nam quam orci, consectetur eu massa vel, porttitor elementum turpis. Nulla eu volutpat libero. Curabitur pretium consectetur nulla. Aliquam bibendum eleme', '', 55.00, 'tomncinno.jpg', 1, '2017-12-07 11:09:28'),
 	(19, 14, 'Brewed Coffee w/ Milk', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin in nisl non vulputate. Nam quam orci, consectetur eu massa vel, porttitor elementum turpis. Nulla eu volutpat libero. Curabitur pretium consectetur nulla. Aliquam bibendum eleme', '', 30.00, 'brewed coffee.jpg', 1, '2017-12-07 11:10:59'),
 	(20, 14, 'Caramel Latte', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin in nisl non vulputate. Nam quam orci, consectetur eu massa vel, porttitor elementum turpis. Nulla eu volutpat libero. Curabitur pretium consectetur nulla. Aliquam bibendum eleme', '', 70.00, 'caramel_flan_latte.jpg', 1, '2017-12-07 11:12:58'),
-	(26, 14, 'White Coffee', 'Lorem ipsum', '', 100.00, 'hot choco.jpg', 1, '2017-12-07 16:44:46'),
+	(26, 14, 'White Coffee', 'Lorem ipsuma', '', 100.00, 'hot choco.jpg', 1, '2017-12-07 16:44:46'),
 	(30, 15, 'White Hot Mocha Latte', 'Lorem ipsum dolor sit amet jud pud pud ba kana ba', '', 145.00, 'mocha latte.jpg', 1, '2017-12-08 17:34:19'),
 	(31, 14, 'Espresso', 'Espresso is coffee brewed by forcing a small amount of nearly boiling water under pressure through finely ground coffee beans.', '', 180.00, 'espresso_830x550.jpg', 1, '2017-12-09 13:57:48');
 /*!40000 ALTER TABLE `items` ENABLE KEYS */;
@@ -95,22 +94,19 @@ CREATE TABLE IF NOT EXISTS `oitem` (
   `oi_qty` int(2) NOT NULL DEFAULT '0',
   `oi_subtotal` float(10,2) NOT NULL DEFAULT '0.00',
   `usr_id` int(8) NOT NULL DEFAULT '0',
+  `oi_status` int(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`oi_id`),
   KEY `item_id` (`item_id`,`order_id`),
   KEY `usr_id` (`usr_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=50000051 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=50000083 DEFAULT CHARSET=latin1;
 
--- Dumping data for table db_sleepnotgo.oitem: 8 rows
+-- Dumping data for table db_sleepnotgo.oitem: 7 rows
 /*!40000 ALTER TABLE `oitem` DISABLE KEYS */;
-INSERT INTO `oitem` (`oi_id`, `order_id`, `item_id`, `oi_qty`, `oi_subtotal`, `usr_id`) VALUES
-	(50000044, 20000040, 17, 1, 45.00, 1),
-	(50000045, 20000040, 30, 1, 145.00, 1),
-	(50000046, 20000041, 17, 10, 450.00, 1),
-	(50000047, 20000042, 17, 1, 45.00, 1),
-	(50000048, 20000043, 18, 1, 55.00, 1),
-	(50000049, 20000044, 17, 1, 45.00, 1),
-	(50000050, 20000045, 17, 1, 45.00, 1),
-	(50000043, 20000039, 18, 1, 55.00, 30);
+INSERT INTO `oitem` (`oi_id`, `order_id`, `item_id`, `oi_qty`, `oi_subtotal`, `usr_id`, `oi_status`) VALUES
+	(50000082, 20000065, 18, 1, 55.00, 1, 0),
+	(50000081, 20000064, 30, 1, 145.00, 1, 0),
+	(50000080, 20000064, 18, 1, 55.00, 1, 1),
+	(50000079, 20000064, 17, 1, 45.00, 1, 1);
 /*!40000 ALTER TABLE `oitem` ENABLE KEYS */;
 
 -- Dumping structure for table db_sleepnotgo.orders
@@ -122,18 +118,13 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `approved_by` int(1) NOT NULL DEFAULT '0',
   `usr_id` int(8) NOT NULL DEFAULT '0',
   PRIMARY KEY (`order_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=20000046 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=20000061 DEFAULT CHARSET=latin1;
 
--- Dumping data for table db_sleepnotgo.orders: 7 rows
+-- Dumping data for table db_sleepnotgo.orders: 4 rows
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
 INSERT INTO `orders` (`order_id`, `order_total`, `created_at`, `order_status`, `approved_by`, `usr_id`) VALUES
-	(20000045, 45.00, '2017-12-15 13:56:17', 0, 0, 1),
-	(20000044, 45.00, '2017-12-13 18:36:20', 0, 0, 1),
-	(20000043, 55.00, '2017-12-13 18:36:06', 0, 0, 1),
-	(20000042, 45.00, '2017-12-13 18:35:40', 0, 0, 1),
-	(20000041, 450.00, '2017-12-13 18:30:01', 0, 0, 1),
-	(20000040, 190.00, '2017-12-13 15:00:11', 0, 0, 1),
-	(20000039, 55.00, '2017-12-13 11:29:38', 0, 0, 30);
+	(20000065, 55.00, '2017-12-20 19:25:47', 0, 0, 1),
+	(20000064, 245.00, '2017-12-20 18:08:03', 0, 0, 1);
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 
 -- Dumping structure for table db_sleepnotgo.users
